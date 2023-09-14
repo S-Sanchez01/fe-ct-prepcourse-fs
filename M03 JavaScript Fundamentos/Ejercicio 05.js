1688abe1520ec94bbfc15c04526ad186b5019e99
S-Sanchez01 <riverossanchessebastian@gmail.com>
2023-09-14 11:49:36 -0500

    PrepCourse

--- M03 JavaScript Fundamentos/Ejercicio 05.js
+++ M03 JavaScript Fundamentos/Ejercicio 05.js
@@ -10,11 +10,7 @@ function esPositivo(num) {
    // Tu código:
 }
 
-function agregarSimboloExclamacion(str) {
-   // Agrega un símbolo de exclamación al final del string "str" y retórnalo
-   // Ejemplo: "hello world" ---> "hello world!"
-   // Tu código:
-}
+
 
 function combinarNombres(nombre, apellido) {
    // Retorna "nombre" y "apellido" combinados en un mismo string pero separados por un espacio.
