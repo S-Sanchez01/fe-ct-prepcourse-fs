1e05509a6aa8b37c0102aa9b1a6c5f9f610022db
S-Sanchez01 <riverossanchessebastian@gmail.com>
2023-09-14 13:26:39 -0500

    TuMensaje

--- M03 JavaScript Fundamentos/Ejercicio 05.js
+++ M03 JavaScript Fundamentos/Ejercicio 05.js
@@ -8,31 +8,60 @@ function esPositivo(num) {
    // Si el número es negativo ---> "Es negativo".
    // Si el número es 0, devuelve false.
    // Tu código:
+   if ( num == 0) return false 
+   if ( num > 0) return "Es positivo"
+   if ( num < 0) return "Es negativo"
 }
 
+function agregarSimboloExclamacion (str) {
+   // agrega un simbolo de exclamacion al final del string "str" y retornalo 
+   // ejemplo: "hello world"  ---> "hello world!"
+   // tu codigo:
+   if ( str === "") {
+      return "¡";
+   } else {
+      return str + "!"
+   }
 
+}
 
 function combinarNombres(nombre, apellido) {
    // Retorna "nombre" y "apellido" combinados en un mismo string pero separados por un espacio.
    // Ejemplo: ("Soy", "Henry") ---> "Soy Henry"
    // Tu código:
+   var combinado =  nombre + " " + apellido
+   return combinado
 }
+
+
 
 function obtenerSaludo(nombre) {
    // Toma el string "nombre" y concatena otra string en la cadena para que tome la siguiente forma:
    // Ejemplo: "Martin" ---> "Hola Martin!"
    // Tu código:
+   return "Hola " + nombre + "!"
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
    // Retornar el área de un rectángulo teniendo su altura y ancho.
    // Tu código:
+   if ( alto > 0 && ancho > 0) {
+      return alto * ancho;
+   } else {
+     return area;
+   }
 }
+
 
 function retornarPerimetro(lado) {
    // La función recibe como argumento la medida de un lado de un cuadrado.
    // Debes retornar su perímetro.
    // Tu código:
+   if (lado > 0) {
+      return lado * 4;
+    } else {
+      return Perimetro;
+    }
 }
 
 function areaDelTriangulo(base, altura) {
@@ -51,6 +80,9 @@ function esVocal(letra) {
    // Si el usuario ingresa un string de más de un caracter debes retornar el mensaje: "Dato incorrecto".
    // Si no es vocal, tambien debe retornar "Dato incorrecto".
    // Tu código:
+   letra = letra.toLowerCase(); 
+
+   
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
